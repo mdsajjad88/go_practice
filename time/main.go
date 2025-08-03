a320c5e097112f88b84d96dfb0345945c24405cb
mdsajjad88 <sajjadhossan608@gmail.com>
2025-08-03 23:19:50 +0600

    current date to add some day are done

--- time/main.go
+++ time/main.go
@@ -16,4 +16,9 @@ func main(){
 	strTime := "31-01-2023"
 	frmStrTime, _ := time.Parse("02-01-2006", strTime)
 	fmt.Println("Formatted String time is ", frmStrTime)	
+	
+	// today with add 1/24 ---2/48 date
+	plusOneDay := times.Add(72* time.Hour)
+	plusOneDayFormat  := plusOneDay.Format("2006/01/02, Monday")
+	fmt.Println(" add time is ", plusOneDayFormat)
 }
