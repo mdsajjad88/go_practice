1a8c9e075115d7232ace7a7ccb3c03cf92523487
mdsajjad88 <sajjadhossan608@gmail.com>
2025-08-03 23:04:25 +0600

    str to time format

--- time/main.go
+++ time/main.go
@@ -5,11 +5,15 @@ import (
 	"time"
 )
 func main(){
-	var time = time.Now()
-	fmt.Println("time is ", time)
+	var times = time.Now()
+	fmt.Println("time is ", times)
 	
-	formatTime := time.Format("2006-01-02, Monday, 15:04:05")
-	formatSecondTime := time.Format("02/01/2006, Monday, 3:04:05 PM")
+	formatTime := times.Format("2006-01-02, Monday, 15:04:05")
+	formatSecondTime := times.Format("02/01/2006, Monday, 3:04:05 PM")
 	fmt.Println("Formatted time is ", formatTime)
 	fmt.Println("Formatted second time is ", formatSecondTime)
+	
+	strTime := "31-01-2023"
+	frmStrTime, _ := time.Parse("02-01-2006", strTime)
+	fmt.Println("Formatted String time is ", frmStrTime)	
 }
