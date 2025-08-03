f27d5e04d7b7ff81e2537c414a149d0c2c379ba9
mdsajjad88 <sajjadhossan608@gmail.com>
2025-08-03 22:49:23 +0600

    time format show with day name

--- time/main.go
+++ time/main.go
@@ -8,6 +8,6 @@ func main(){
 	var time = time.Now()
 	fmt.Println("time is ", time)
 	
-	formatTime := time.Format("2006-01-02")
+	formatTime := time.Format("2006-01-02, Monday, 15:04:05")
 	fmt.Println("Formatted time is ", formatTime)
 }
