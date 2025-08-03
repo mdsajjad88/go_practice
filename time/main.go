10a135c41e2ad81914cc80b2b68eb1c77d20f2ef
mdsajjad88 <sajjadhossan608@gmail.com>
2025-08-03 22:53:04 +0600

    show time another format

--- time/main.go
+++ time/main.go
@@ -9,5 +9,7 @@ func main(){
 	fmt.Println("time is ", time)
 	
 	formatTime := time.Format("2006-01-02, Monday, 15:04:05")
+	formatSecondTime := time.Format("02/01/2006, Monday, 3:04:05 PM")
 	fmt.Println("Formatted time is ", formatTime)
+	fmt.Println("Formatted second time is ", formatSecondTime)
 }
