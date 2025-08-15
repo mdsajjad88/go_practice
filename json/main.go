1717a993d88a097716dedf6df665f61ffd070014
mdsajjad88 <sajjadhossan608@gmail.com>
2025-08-15 16:23:56 +0600

    start json in golang practice

---
+++ json/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name   string `json:"name"`
+	Mobile int    `json:"mobile"`
+	Age    int    `json:"age"`
+	Gender string `json:"gender"`
+}
+
+func main() {
+	user := Person{Name: "Rokon", Mobile: 1548758965, Age: 24, Gender: "Male"}
+	fmt.Println("user data is : ", user)
+	
+	jsonUser, err := json.Marshal(user)
+	if err != nil{
+		fmt.Println("Something is wrong ", err)
+	}
+	fmt.Println("Json user data is :", string(jsonUser))
+}
