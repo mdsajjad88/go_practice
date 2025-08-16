cce2ac4308b99cb0dab50bf785f3ce5b142dd2ad
mdsajjad88 <sajjadhossan608@gmail.com>
2025-08-16 10:38:27 +0600

    practice golang json marshal unmarshal

--- json/main.go
+++ json/main.go
@@ -15,10 +15,19 @@ type Person struct {
 func main() {
 	user := Person{Name: "Rokon", Mobile: 1548758965, Age: 24, Gender: "Male"}
 	fmt.Println("user data is : ", user)
-	
+
 	jsonUser, err := json.Marshal(user)
-	if err != nil{
+	if err != nil {
 		fmt.Println("Something is wrong ", err)
 	}
 	fmt.Println("Json user data is :", string(jsonUser))
+
+	var personData Person
+
+	errs := json.Unmarshal(jsonUser, &personData)
+	if errs != nil {
+		fmt.Println("error in unmarshal is = ", errs)
+		return
+	}
+	fmt.Println("Hello person data is = ", personData)
 }
